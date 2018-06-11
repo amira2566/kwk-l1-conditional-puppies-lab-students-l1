812d704c371cc5b4e5aefe797fa005c45d582017
amira2566 <braemira2@icloud.com>
2018-06-11 16:11:14 +0000

    automatically backed up by learn

---
+++ class.rb
@@ -0,0 +1,21 @@
+class Dog
+  def name=(dog_name)
+    @this_dogs_name= dog_name
+  end 
+  def name
+    @this_dogs_name
+  end
+  def breed=(dog_breed)
+    @this_dogs_breed= dog_breed
+  end 
+  def breed
+    @this_dogs_breed
+  end
+  def bark
+    puts "woof!"
+  end
+end
+izzy=Dog.new
+puts izzy.name="izzy"
+puts izzy.breed= "american bulldog"
+
